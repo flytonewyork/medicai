7cb93469fd1e0132df47f2df3fbbbb3d54bdca53
Thomas Hu <thomas.hu94@gmail.com>
2023-04-10 19:29:22 +0800

    added clerk sign button

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,5 @@
 import { createTRPCRouter } from "~/server/api/trpc";
-import { postsRouter } from "~/server/api/routers/example";
+import { postsRouter } from "~/server/api/routers/posts";
 
 /**
  * This is the primary router for your server.
